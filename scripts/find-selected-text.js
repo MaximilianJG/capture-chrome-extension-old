cb946d1aed946705d96ca9d3f946760b6d6cc374
Maximilian Glasmacher <maximilian@glasmacher.com>
2020-09-01 12:15:07 +0200

    Fetching everything, API set up, have to allow cors to api in capture cap

--- scripts/find-selected-text.js
+++ scripts/find-selected-text.js
@@ -1,6 +1,54 @@
-function selectText() {
+// const selectText = () => {
+//   const selectedText = window.getSelection().toString();
+//   return selectedText
+// }
+
+const fetchData = () => {
   const selectedText = window.getSelection().toString();
-  console.log(selectedText)
+  const url_of_quote = window.location.href
+  const title = document.querySelector("h1").innerText
+  const website = window.location.hostname
+  const url_of_website = url_of_quote
+
+  return {
+    content: selectedText,
+    url_of_quote: url_of_quote,
+    title: title,
+    website: website,
+    url_of_website: url_of_website
+  }
 }
 
-selectText()
+console.log(fetchData())
+
+const sendData = (data) => {
+  const url = "https://capture-maximilianjg.herokuapp.com/api/v1/quotes"
+
+  const params = {
+    quote: {
+      content: data.content,
+      url_of_quote: data.url_of_quote,
+    },
+    source: {
+      title: data.title,
+      website: data.website,
+      url_of_website: data.url_of_website
+    }
+  }
+
+  const options = {
+    method: "POST",
+    mode: "no-cors",
+    body: JSON.stringify(params)
+  }
+
+
+
+  console.log(params)
+
+  fetch(url, options)
+  .then(response => response.json())
+  .then(data => console.log(data))
+}
+
+sendData(fetchData())
