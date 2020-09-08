922689c2bb912cf297ee1f774790b59d569aca88
Maximilian Glasmacher <maximilian@glasmacher.com>
2020-09-08 13:45:20 +0200

    restructure

--- popup.js
+++ popup.js
@@ -1,10 +1,14 @@
 const listenClick = () => {
   const button = document.getElementById('capture-this-btn');
   button.addEventListener('click', () => {
+
     chrome.tabs.executeScript({
-      file: 'scripts/find-selected-text.js'
+      file: 'scripts/find-send-data.js'
+    })
+    chrome.tabs.executeScript({
+      file: 'scripts/highlight-text.js'
     });
-    window.close()
+    window.close();
   })
 }
 
